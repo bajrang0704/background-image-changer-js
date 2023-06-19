8102f750b02a272d660778207bab735bcb52a460
bajrang0704 <104763943+bajrang0704@users.noreply.github.com>
2023-06-19 23:52:58 +0530

    imgchanger.js

--- imgchanger.js
+++ imgchanger.js
@@ -1,9 +1,12 @@
 function Charminar(){
     document.getElementById("background").style.backgroundImage="url('https://images.unsplash.com/photo-1551161242-b5af797b7233?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=851&q=80')";
+    document.getElementById("name").textContent="Charminar";
 }
 function tajmahal(){
     document.getElementById("background").style.backgroundImage="url('https://images.unsplash.com/photo-1564507592333-c60657eea523?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80')";
+    document.getElementById("name").textContent="Tajmahal";
 }
 function RedFort(){
     document.getElementById("background").style.backgroundImage="url('https://plus.unsplash.com/premium_photo-1661919589683-f11880119fb7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80')";
+    document.getElementById("name").textContent="Red Fort";
 }
